ee15295a9482ebe82bb6e5eea92076b890ef42b8
sivagugan30 <sivagugan.98@gmail.com>
2024-12-26 21:56:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,6 @@ os.environ["OPENAI_API_KEY"] = st.secrets["KEY"]
 # App title
 st.title("Ask-RAG-LLM")
 
-st.write(os.environ["OPENAI_API_KEY"])
-
 # Sidebar for topic selection and document upload
 st.sidebar.header("Options")
 topics = ["Religion", "Soccer", "ADHD Help"]
