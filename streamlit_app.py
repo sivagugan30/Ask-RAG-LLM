c5555cf71695515ec72fbaa0814d77c6739c76e6
sivagugan30 <sivagugan.98@gmail.com>
2024-12-28 02:34:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ os.environ["OPENAI_API_KEY"] = st.secrets["KEY"]
 
 # Predefined list of JSON file paths
 json_files = [
-    "famous_five_1.json",
+    "sqlite/famous_five_1.json",
     "famous_five_2.json",
     "famous_five_3.json",
     "famous_five_4.json",
