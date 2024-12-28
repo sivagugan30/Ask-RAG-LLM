cf912b31f44797edf00f5600acd341b5babdfb40
sivagugan30 <sivagugan.98@gmail.com>
2024-12-27 20:28:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,35 +24,33 @@ def load_json_files(json_files):
     embeddings = []
     
     for json_file in json_files:
-        st.write(f"Loading JSON data from: {json_file}")
+        #st.write(f"Loading JSON data from: {json_file}")
         try:
             # Open and read the JSON file
             with open(json_file, 'r') as file:
                 json_data = json.load(file)  # Parse the JSON content
-            st.write(f"Successfully loaded JSON data from {json_file}")
+            #st.write(f"Successfully loaded JSON data from {json_file}")
         except Exception as e:
             st.write(f"Failed to load JSON data from {json_file}: {e}")
             continue  # Skip this file if reading fails
 
         # Process the data from the JSON
         try:
-            st.write("Processing JSON data.")
+            #st.write("Processing JSON data.")
             ids.extend(json_data["ids"])  # Append data to the existing list
             documents.extend(json_data["documents"])
             metadata.extend(json_data["metadata"])
             embeddings.extend(json_data["embeddings"])
-            st.write(f"Processed data with {len(json_data['ids'])} rows successfully.")
+            #st.write(f"Processed data with {len(json_data['ids'])} rows successfully.")
         except KeyError as e:
             st.write(f"Error: Key {e} not found in the JSON data.")
         except Exception as e:
             st.write(f"Error processing the JSON data: {e}")
 
     # Convert embeddings to a NumPy array
-    st.write("Converting embeddings to a NumPy array.")
     embeddings_array = np.array(embeddings)
 
     # Combine all data into a dictionary
-    st.write("Combining all data into a single dictionary.")
     vector_dict = {
         "ids": ids,
         "documents": documents,
@@ -60,9 +58,6 @@ def load_json_files(json_files):
         "embeddings": embeddings_array
     }
 
-    st.write("Data loading complete.")
-    st.write(f"Shape of embeddings: {vector_dict['embeddings'].shape}")
-    st.write(f"Number of IDs: {len(vector_dict['ids'])}")
 
     return vector_dict
 
