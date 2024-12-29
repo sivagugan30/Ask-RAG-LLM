77c558292f964e4e130610e62e8909177942d681
sivagugan30 <sivagugan.98@gmail.com>
2024-12-29 03:30:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -202,7 +202,7 @@ tabs = st.tabs(["Document Embedding", "RAG Chatbot"])
 
 
 with tabs[0]:
-    st.markdown("<p style='font-size: 20px;'>Convert <strong><em>Text</em></strong> to <strong><em>Numerical Representation</em></strong></p>", unsafe_allow_html=True)
+    st.markdown("<p style='font-size: 40px;'>Convert <strong><em>Text</em></strong> to <strong><em>Numerical Representation</em></strong></p>", unsafe_allow_html=True)
 
 
     
