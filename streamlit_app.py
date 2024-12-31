2adc79868f81118dd0b660a8849767ba5fdd48c3
sivagugan30 <sivagugan.98@gmail.com>
2024-12-31 04:36:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,13 +16,13 @@ os.environ["OPENAI_API_KEY"] = st.secrets["KEY"]
 # Predefined list of JSON file paths
 json_files = [
     "tech_1.json",
-    "tech_2.json",
-    "tech_3.json",
-    "tech_4.json",
-    "tech_5.json",
-    "tech_6.json",
-    "tech_7.json",
-    "tech_8.json"
+    "tech_2.json"
+    # "tech_3.json",
+    # "tech_4.json",
+    # "tech_5.json",
+    # "tech_6.json",
+    # "tech_7.json",
+    # "tech_8.json"
 ]
 
 # Sidebar navigation
