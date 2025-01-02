3e0b8abbdf00a40e56c5e45dae55c8dc8016abcb
sivagugan30 <sivagugan.98@gmail.com>
2025-01-02 15:03:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -214,17 +214,10 @@ elif options == "How RAG works?":
     
                     st.write("_First, Cosine Similarity is applied to the embeddings of user prompt and the external database to **RETRIEVE** the top 3 relevant results._")
 
-                    st.code("""
-                        distances = Cosine similarity score (0-1), where 1 means a perfect match. Higher values indicate more relevance
-                        
-                        documents = The relevant text content from the external database
-                        
-                        metadata = Extra details about the documents, such as their source
-                    """, language="markdown")
-
-
-
-                    
+                    st.code(' distances = Cosine similarity score (0-1), where 1 means a perfect match. Higher values indicate more relevance')
+                    st.code(' documents = Relevant text content from the external database')
+                    st.code(' metadata = Extra details about the documents, such as their source')
+                                            
                     results1 = {
                                 "distances" : results["distances"],
                                 "documents" : results["documents"],
