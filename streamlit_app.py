a7dbcffa30351a0f9aac70abeb849afe666df298
sivagugan30 <sivagugan.98@gmail.com>
2025-01-05 12:37:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ elif options == "Chatbot":
         st.session_state.messages = []
 
     # User input for the query
-    user_input = st.chat_input("Ask me anything about the buzzing tech topics:")
+    user_input = st.chat_input("Ask me anything about the latest trends in Tech!")
 
     if user_input:
         # Add the user input to the conversation history
